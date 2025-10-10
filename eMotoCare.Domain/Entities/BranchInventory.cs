81549815c05d5d569407aa5a784e1a53595a1dbf
TuanNguyen <kenjizukochannel@gmail.com>
2025-10-10 17:09:50 +0700

    Fix relationship

--- eMotoCare.Domain/Entities/BranchInventory.cs
+++ eMotoCare.Domain/Entities/BranchInventory.cs
@@ -5,12 +5,10 @@ namespace eMotoCare.Domain.Entities
     {
         public Guid Id { get; set; }
         public Guid BranchId { get; set; }
-        public Guid PartItemId { get; set; }
         public int Quantity { get; set; }
         public DateTime LastUpdated { get; set; }
 
         public virtual Branch? Branch { get; set; }
-        public virtual PartItem? PartItem { get; set; }
 
     }
 }
--- eMotoCare.Domain/Entities/PartItem.cs
+++ eMotoCare.Domain/Entities/PartItem.cs
@@ -14,9 +14,11 @@ namespace eMotoCare.Domain.Entities
         public DateTime WarrantyExpire { get; set; }
         public Guid? ExportNoteId { get; set; }
         public Guid? ImportNoteId { get; set; }
+        public Guid BranchInventoryId { get; set; }
 
         public virtual Part? Part { get; set; }
         public virtual ExportNote? ExportNote { get; set; }
         public virtual ImportNote? ImportNote { get; set; }
+        public virtual BranchInventory? BranchInventory { get; set; }
     }
 }
