ec26786fa78a776a7180020a4739adff10c599fd
nghiatlm <nghiatlmqe170244@fpt.edu.vn>
2025-10-16 15:03:32 +0700

    update DI

--- eMotoCare.DAL/Configuration/DependencyInjection.cs
+++ eMotoCare.DAL/Configuration/DependencyInjection.cs
@@ -1,5 +1,32 @@
-
 using eMotoCare.DAL.Base;
+using eMotoCare.DAL.Repositories.AccountRepository;
+using eMotoCare.DAL.Repositories.AppointmentRepository;
+using eMotoCare.DAL.Repositories.BatteryCheckRepository;
+using eMotoCare.DAL.Repositories.CampaignDetailRepository;
+using eMotoCare.DAL.Repositories.CampaignRepository;
+using eMotoCare.DAL.Repositories.CustomerRepository;
+using eMotoCare.DAL.Repositories.EVCheckDetailRepository;
+using eMotoCare.DAL.Repositories.EVCheckRepository;
+using eMotoCare.DAL.Repositories.ExportNoteRepository;
+using eMotoCare.DAL.Repositories.ImportNoteRepository;
+using eMotoCare.DAL.Repositories.MaintenancePlanRepository;
+using eMotoCare.DAL.Repositories.MaintenanceStageDetailRepository;
+using eMotoCare.DAL.Repositories.MaintenanceStageRepository;
+using eMotoCare.DAL.Repositories.ModelPartTypeRepository;
+using eMotoCare.DAL.Repositories.ModelRepository;
+using eMotoCare.DAL.Repositories.PartItemRepository;
+using eMotoCare.DAL.Repositories.PartRepository;
+using eMotoCare.DAL.Repositories.PartTypeRepository;
+using eMotoCare.DAL.Repositories.PaymentRepository;
+using eMotoCare.DAL.Repositories.PriceServiceRepository;
+using eMotoCare.DAL.Repositories.RMADetailRepository;
+using eMotoCare.DAL.Repositories.RMARepository;
+using eMotoCare.DAL.Repositories.ServiceCenterInventoryRepository;
+using eMotoCare.DAL.Repositories.ServiceCenterRepository;
+using eMotoCare.DAL.Repositories.StaffRepository;
+using eMotoCare.DAL.Repositories.VehiclePartItemRepository;
+using eMotoCare.DAL.Repositories.VehicleRepository;
+using eMotoCare.DAL.Repositories.VehicleStageRepository;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace eMotoCare.DAL.Configuration
@@ -11,6 +38,34 @@ namespace eMotoCare.DAL.Configuration
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
+            services.AddScoped<IAccountRepository, AccountRepository>();
+            services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+            services.AddScoped<IBatteryCheckRepository, BatteryCheckRepository>();
+            services.AddScoped<ICampaignDetailRepository, CampaignDetailRepository>();
+            services.AddScoped<ICampaignRepository, CampaignRepository>();
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<IEVCheckDetailRepository, EVCheckDetailRepository>();
+            services.AddScoped<IEVCheckRepository, EVCheckRepository>();
+            services.AddScoped<IExportNoteRepository, ExportNoteRepository>();
+            services.AddScoped<IImportNoteRepository, ImportNoteRepository>();
+            services.AddScoped<IMaintenancePlanRepository, MaintenancePlanRepository>();
+            services.AddScoped<IMaintenanceStageDetailRepository, MaintenanceStageDetailRepository>();
+            services.AddScoped<IMaintenanceStageRepository, MaintenanceStageRepository>();
+            services.AddScoped<IModelPartTypeRepository, ModelPartTypeRepository>();
+            services.AddScoped<IModelRepository, ModelRepository>();
+            services.AddScoped<IPartItemRepository, PartItemRepository>();
+            services.AddScoped<IPartRepository, PartRepository>();
+            services.AddScoped<IPartTypeRepository, PartTypeRepository>();
+            services.AddScoped<IPaymentRepository, PaymentRepository>();
+            services.AddScoped<IPriceServiceRepository, PriceServiceRepository>();
+            services.AddScoped<IRMADetailRepository, RMADetailRepository>();
+            services.AddScoped<IRMARepository, RMARepository>();
+            services.AddScoped<IServiceCenterInventoryRepository, ServiceCenterInventoryRepository>();
+            services.AddScoped<IServiceCenterRepository, ServiceCenterRepository>();
+            services.AddScoped<IStaffRepository, StaffRepository>();
+            services.AddScoped<IVehiclePartItemRepository, VehiclePartItemRepository>();
+            services.AddScoped<IVehicleRepository, VehicleRepository>();
+            services.AddScoped<IVehicleStageRepository, VehicleStageRepository>();
 
             return services;
         }
--- eMotoCare.DAL/Repositories/AccountRepository/IAccountRepository.cs
+++ eMotoCare.DAL/Repositories/AccountRepository/IAccountRepository.cs
@@ -3,5 +3,9 @@ using eMotoCare.DAL.Base;
 
 namespace eMotoCare.DAL.Repositories.AccountRepository
 {
-    public interface IAccountRepository : IGenericRepository<Account> { }
+    public interface IAccountRepository : IGenericRepository<Account>
+    {
+        Task<Account> FindByPhone(string phone);
+        Task<Account> FindByEmail(string email);
+    }
 }
