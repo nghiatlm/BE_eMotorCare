40d9999497cf29522d0450b9adbf3a151578aff6
nghiatlm <nghiatlmqe170244@fpt.edu.vn>
2025-10-17 15:34:50 +0700

    add cors

--- BE_eMotoCare.API/Program.cs
+++ BE_eMotoCare.API/Program.cs
@@ -56,6 +56,14 @@ builder
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "AllowExpoApp",
+        policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+    );
+});
+
 var app = builder.Build();
 
 // if (app.Environment.IsDevelopment())
@@ -67,6 +75,7 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.UseAppExceptionHandler();
+app.UseCors("AllowExpoApp");
 
 app.UseMiddleware<JwtMiddleware>();
 app.UseAuthentication();
