787605476f84fbf44da3d53cf582cb650befb894
thinhnpse172990 <thinhnpse172990@fpt.edu.vn>
2025-11-01 00:38:17 +0700

    fix(vehiclestage): update response vehicle

--- eMotoCare.BO/DTO/Responses/VehicleStageResponse.cs
+++ eMotoCare.BO/DTO/Responses/VehicleStageResponse.cs
@@ -10,6 +10,7 @@ namespace eMotoCare.BO.DTO.Responses
         public int ActualMaintenanceMileage { get; set; }
         public MaintenanceUnit ActualMaintenanceUnit { get; set; }
         public Guid VehicleId { get; set; }
+        public VehicleResponse? Vehicle { get; set; }
         public DateTime DateOfImplementation { get; set; }
         public VehicleStageStatus Status { get; set; }
         public DateTime CreatedAt { get; set; }
