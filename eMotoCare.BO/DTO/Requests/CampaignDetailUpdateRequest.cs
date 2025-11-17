1bd5224760049f883695f70be70a179ec8e0d07a
TuanNguyen <kenjizukochannel@gmail.com>
2025-11-17 12:42:20 +0700

    crud

---
+++ eMotoCare.BO/DTO/Requests/CampaignDetailUpdateRequest.cs
@@ -0,0 +1,35 @@
+﻿
+
+using eMotoCare.BO.Entities;
+using eMotoCare.BO.Enum;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eMotoCare.BO.DTO.Requests
+{
+    public class CampaignDetailUpdateRequest
+    {
+
+
+        public Guid? CampaignId { get; set; }
+
+
+        public string? Description { get; set; }
+
+
+        public Guid? PartId { get; set; }
+
+
+
+        public CampaignActionType? ActionType { get; set; }
+
+
+        public string? Note { get; set; }
+
+ 
+        public bool? IsMandatory { get; set; }
+
+       
+        public int? EstimatedTime { get; set; }
+    }
+}
--- eMototCare.BLL/Mappers/CampaignDetailMapper.cs
+++ eMototCare.BLL/Mappers/CampaignDetailMapper.cs
@@ -3,7 +3,7 @@
 using AutoMapper;
 using eMotoCare.BO.DTO.Responses;
 using eMotoCare.BO.Entities;
-
+using eMotoCare.BO.DTO.Requests;
 namespace eMototCare.BLL.Mappers
 {
     public class CampaignDetailMapper : Profile
@@ -11,6 +11,8 @@ namespace eMototCare.BLL.Mappers
         public CampaignDetailMapper()
         {
             CreateMap<CampaignDetail, CampaignDetailResponse>();
+            CreateMap<CampaignDetailRequest, CampaignDetail>();
+            CreateMap<CampaignDetailUpdateRequest, CampaignDetail>();
         }
     }
 }
