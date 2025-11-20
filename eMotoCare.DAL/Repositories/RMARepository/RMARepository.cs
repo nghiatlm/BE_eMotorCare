3e0eb93a4db110d65d02702727ca79692d8f6090
TuanNguyen <kenjizukochannel@gmail.com>
2025-11-20 19:20:06 +0700

    add filter

--- eMotoCare.DAL/Repositories/RMARepository/RMARepository.cs
+++ eMotoCare.DAL/Repositories/RMARepository/RMARepository.cs
@@ -20,6 +20,7 @@ namespace eMotoCare.DAL.Repositories.RMARepository
              string? returnAddress,
              RMAStatus? status,
              Guid? createdById,
+             Guid? serviceCenterId,
              int page,
              int pageSize
         )
@@ -82,6 +83,13 @@ namespace eMotoCare.DAL.Repositories.RMARepository
             {
                 var endDateInclusive = toDate.Value.Date.AddDays(1);
                 q = q.Where(x => x.RMADate < endDateInclusive);
+            }
+
+            if (serviceCenterId.HasValue)
+            {
+                q = q.Where(x => x.RMADetails.Any(rd =>
+                    rd.EVCheckDetail.EVCheck.Appointment.ServiceCenterId == serviceCenterId.Value
+                ));
             }
 
 
