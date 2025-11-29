0485e43dcb6c6eddef65574f2fef8dfc552894d2
TuanNguyen <kenjizukochannel@gmail.com>
2025-11-29 23:25:59 +0700

    fix status

--- eMototCare.BLL/Services/ExportServices/ExportService.cs
+++ eMototCare.BLL/Services/ExportServices/ExportService.cs
@@ -109,9 +109,6 @@ namespace eMototCare.BLL.Services.ExportServices
                         // Update trạng thái hoặc exportNoteId tùy nghiệp vụ
                         // partItem.ExportNoteId = exportNoteId;
                         partItem.ServiceCenterInventoryId = null;
-                        partItem.Quantity = 0;
-                        partItem.Status = PartItemStatus.IN_ACTIVE;
-                        partItem.Part.Quantity -= 1;
                         await _unitOfWork.PartItems.UpdateAsync(partItem);
                     }
                 }
