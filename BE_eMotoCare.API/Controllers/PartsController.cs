cc5347d25d29bb49f696ca578df9dcadd0d5afcf
thinhnpse172990 <thinhnpse172990@fpt.edu.vn>
2025-11-30 00:56:12 +0700

    fix(author): fix somethings

--- BE_eMotoCare.API/Controllers/PartsController.cs
+++ BE_eMotoCare.API/Controllers/PartsController.cs
@@ -1,13 +1,13 @@
-﻿using eMotoCare.BO.DTO.ApiResponse;
+﻿using BE_eMotoCare.API.Realtime.Services;
+using eMotoCare.BO.DTO.ApiResponse;
 using eMotoCare.BO.DTO.Requests;
 using eMotoCare.BO.DTO.Responses;
+using eMotoCare.BO.DTO.Responses.Labels;
 using eMotoCare.BO.Enum;
 using eMotoCare.BO.Pages;
 using eMototCare.BLL.Services.PartServices;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using BE_eMotoCare.API.Realtime.Services;
-using eMotoCare.BO.DTO.Responses.Labels;
 
 namespace BE_eMotoCare.API.Controllers
 {
@@ -36,7 +36,15 @@ namespace BE_eMotoCare.API.Controllers
             [FromQuery] int pageSize = 10
         )
         {
-            var data = await _partService.GetPagedAsync(partTypeId, code, name, status, quantity, page, pageSize);
+            var data = await _partService.GetPagedAsync(
+                partTypeId,
+                code,
+                name,
+                status,
+                quantity,
+                page,
+                pageSize
+            );
             return Ok(
                 ApiResponse<PageResult<PartResponse>>.SuccessResponse(
                     data,
@@ -46,16 +54,11 @@ namespace BE_eMotoCare.API.Controllers
         }
 
         [HttpGet("{id}")]
-        [Authorize(Roles = "ROLE_MANAGER,ROLE_STAFF,ROLE_STOREKEEPER")]
+        [Authorize(Roles = "ROLE_MANAGER,ROLE_STAFF,ROLE_STOREKEEPER,ROLE_TECHNICIAN")]
         public async Task<IActionResult> GetById(Guid id)
         {
             var item = await _partService.GetByIdAsync(id);
-            return Ok(
-                ApiResponse<PartResponse>.SuccessResponse(
-                    item,
-                    "Lấy Part thành công"
-                )
-            );
+            return Ok(ApiResponse<PartResponse>.SuccessResponse(item, "Lấy Part thành công"));
         }
 
         [HttpPost]
@@ -63,9 +66,7 @@ namespace BE_eMotoCare.API.Controllers
         public async Task<IActionResult> Create([FromBody] PartRequest request)
         {
             var id = await _partService.CreateAsync(request);
-            return Ok(
-                ApiResponse<object>.SuccessResponse(new { id }, "Tạo Part thành công")
-            );
+            return Ok(ApiResponse<object>.SuccessResponse(new { id }, "Tạo Part thành công"));
         }
 
         [HttpDelete("{id}")]
@@ -83,38 +84,41 @@ namespace BE_eMotoCare.API.Controllers
         {
             await _partService.UpdateAsync(id, request);
             await _notifier.NotifyUpdateAsync("Part", new { Id = id });
-            return Ok(
-                ApiResponse<string>.SuccessResponse(null, "Cập nhật Part thành công")
-            );
+            return Ok(ApiResponse<string>.SuccessResponse(null, "Cập nhật Part thành công"));
         }
 
         [HttpGet("by-part-type/{partTypeId}")]
         public async Task<IActionResult> GetByPartType(Guid partTypeId)
         {
             var items = await _partService.GetByPartType(partTypeId);
-            return items != null ? Ok(
-                ApiResponse<List<PartLabel>>.SuccessResponse(
-                    items,
-                    "Lấy danh sách Part theo loại thành công"
+            return items != null
+                ? Ok(
+                    ApiResponse<List<PartLabel>>.SuccessResponse(
+                        items,
+                        "Lấy danh sách Part theo loại thành công"
+                    )
                 )
-            ) : NotFound(
-                ApiResponse<string>.BadRequest("Không tìm thấy Part theo loại")
-            );
+                : NotFound(ApiResponse<string>.BadRequest("Không tìm thấy Part theo loại"));
         }
 
         [HttpGet("by-model-and-type")]
         [Authorize(Roles = "ROLE_MANAGER,ROLE_STAFF,ROLE_STOREKEEPER,ROLE_ADMIN,ROLE_TECHNICIAN")]
-        public async Task<IActionResult> GetPartsByModelandType([FromQuery] Guid model, [FromQuery] Guid partTypeId)
+        public async Task<IActionResult> GetPartsByModelandType(
+            [FromQuery] Guid model,
+            [FromQuery] Guid partTypeId
+        )
         {
             var items = await _partService.GetPartsByModelandType(model, partTypeId);
-            return items != null ? Ok(
-                ApiResponse<List<PartLabel>>.SuccessResponse(
-                    items,
-                    "Lấy danh sách Part theo model và loại thành công"
+            return items != null
+                ? Ok(
+                    ApiResponse<List<PartLabel>>.SuccessResponse(
+                        items,
+                        "Lấy danh sách Part theo model và loại thành công"
+                    )
                 )
-            ) : NotFound(
-                ApiResponse<string>.BadRequest("Không tìm thấy Part theo model và loại")
-            );
+                : NotFound(
+                    ApiResponse<string>.BadRequest("Không tìm thấy Part theo model và loại")
+                );
         }
     }
 }
