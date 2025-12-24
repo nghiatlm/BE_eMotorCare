620736aeac3b4342b597e625d535f155854d42eb
TuanNguyen <kenjizukochannel@gmail.com>
2025-12-24 15:27:51 +0700

    fix

---
+++ eMotoCare.BO/DTO/Requests/GetAvailableTechnicianRequest.cs
@@ -0,0 +1,12 @@
+﻿
+
+using eMotoCare.BO.Enums;
+
+namespace eMotoCare.BO.DTO.Requests
+{
+    public class GetAvailableTechnicianRequest
+    {
+        public SlotTime SlotTime { get; set; }
+        public DateTime AppointmentDate { get; set; }
+    }
+}
